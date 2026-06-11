31d46f0d82ab5a38094e7e8d2f451eae7f5f55a9
Avirup Ghosh <avirup.ghosh@avirup.ghosh-C02C90KGMD6Q>
2026-06-11 09:11:54 +0530

    fc

--- initial.sh
+++ initial.sh
@@ -2,7 +2,7 @@
 
 export MY_SANDBOX_API_URL=http://127.0.0.1:8000
 export MY_SANDBOX_API_KEY=test-key-12345
-export OPENROUTER_API_KEY=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
+
 export OPENROUTER_MODEL=nvidia/nemotron-3-super-120b-a12b:free
 export USE_OLLAMA=1
 export SANDBOX_ISOLATION=docker
